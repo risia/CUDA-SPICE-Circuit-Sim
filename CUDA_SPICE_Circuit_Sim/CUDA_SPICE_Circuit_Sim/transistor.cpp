c4e2e13e2b66db8386af2861f63246311265c84d
Angelina Risi <risia@users.noreply.github.com>
2018-11-19 17:48:03 -0500

    Milestone 1 Presentation

--- CUDA_SPICE_Circuit_Sim/CUDA_SPICE_Circuit_Sim/transistor.cpp
+++ CUDA_SPICE_Circuit_Sim/CUDA_SPICE_Circuit_Sim/transistor.cpp
@@ -1,5 +1,7 @@
 #include "transistor.h"
 
+// WIP
+
 // guessed voltage matrix vMat as input
 // keep solved values separate.
 float calcId(Transistor* T, float* vMat) {
@@ -16,14 +18,26 @@ float calcId(Transistor* T, float* vMat) {
 	if (Vg - Vs <= vth && T->model->type == 'n') return 0.0f;
 	if (Vg - Vs >= vth && T->model->type == 'p') return 0.0f;
 
-	float k = T->model->u0 * 3.9 * PERMITTIVITY / (T->model->tox * 100.f);
+	float k = (T->w / T->l) * T->model->u0 * (3.9f * PERMITTIVITY / (T->model->tox * 100.f));
 
-	// saturation
+	// saturation region
 	if (Vd > (Vg - vth)) {
 		return (0.5f * k * (Vg - vth) * (Vg - vth));
 	}
-	// "linear"
+	// "linear" region
 	else {
 		return (k * ((Vg - vth) * Vd) + (0.5 * Vd * Vd));
 	}
+}
+
+void MOS_toMat(Transistor* T, float** gMat, float* iMat, float* vGuess) {
+	// Load guessed node voltages
+	float Vg = 0.0f;
+	if (T->g > 0) Vg = vGuess[T->g - 1];
+	float Vs = 0.0f;
+	if (T->s > 0) Vs = vGuess[T->s - 1];
+	float Vd = 0.0f;
+	if (T->d > 0) Vs = vGuess[T->d - 1];
+
+
 }
