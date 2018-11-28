f1009193cfe211fb0c86a15d9d639400a5c510f7
Angelina Risi <risia@users.noreply.github.com>
2018-11-28 17:39:27 -0500

    Updated Presentation

--- CUDA_SPICE_Circuit_Sim/CUDA_SPICE_Circuit_Sim/main.cpp
+++ CUDA_SPICE_Circuit_Sim/CUDA_SPICE_Circuit_Sim/main.cpp
@@ -3,7 +3,7 @@
 int main() {
 	Netlist netlist;
 
-	char* file = "C:/Users/Angelinia/Documents/CIS 565/CUDA-SPICE-Circuit-Sim/test_spi/P_test1.spi";
+	char* file = "C:/Users/Angelinia/Documents/CIS 565/CUDA-SPICE-Circuit-Sim/test_spi/Bigger_test.spi";
 	parseNetlist(file, netlist);
 
 	op(netlist);
